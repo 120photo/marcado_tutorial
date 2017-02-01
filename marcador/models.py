38369010860ae3dc6fef87b917214924b1e6d1e5
Jose Juarez <jose@juarez.me>
2017-01-31 20:54:17 -0500

    started work on models

--- marcador/models.py
+++ marcador/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+        ordering = ['name']
+
+
+class Bookmark(models.Model):
+    url = models.URLField()
+    title = models.CharField('title', max_length=255)
+    description = models.TextField('description', blank=True)
+    is_public = models.BooleanField('public', default=True)
+    date_created = models.DateTimeField('date created')
+    date_updated = models.DateTimeField('date updated')
+    owner = models.ForeignKey(User, verbose_name='owner', related_names='bookmarks')
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        verbose_name = 'bookmark'
+        verbose_name_plural = 'bookmarks'
+        ordering = ['-date_created']
